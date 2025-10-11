47e6ec77b2b9b70cb829efd5e0de5409959d8ec1
mnapo <napolimatiase@gmail.com>
2025-10-11 09:48:37 -0300

    update Appt and register its RootNavigator component

---
+++ app/navigation/RootNavigator.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useAuth } from "../hooks/useAuth";
+import LoginScreen from "../screens/LoginScreen";
+import HomeScreen from "../screens/HomeScreen";
+import GameScreen from "../screens/GameScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  const { user } = useAuth();
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Game" component={GameScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+}
